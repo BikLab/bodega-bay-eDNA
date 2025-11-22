82ffc8b7d46fade190abf68fe7dbe7403cf6b2db
Ese973 <gas51591@uga.edu>
2025-11-22 18:31:31 -0500

    update 18S libraries

---
+++ R-scripts/18S-main.r
@@ -0,0 +1,14 @@
+###################### Libraries ######################
+library(tidyverse) 
+library(qiime2R) # Integrating QIIME2 and R for data visualization and analysis using qiime2R.
+library(phyloseq) # Handling and analysis of high-throughput microbiome census data.
+library(ggh4x) # ggh4x package is a ggplot2 extension package.
+library(cowplot) # Simple add-on to ggplot2; provides additional features to improve graphic quality.  
+library(ggpubr) # Provides some easy-to-use functions to customize ggplot2 graphics.
+library(decontam) # Decontam package is used to identify and remove contaminants from microbiome data.
+library(fantaxtic) # Provides functions to work with taxonomic data in R.
+library(microbiome) # Tools for microbiome analysis
+library(gridExtra) # Provides functions to arrange multiple grid-based figures on a single page.
+library(microViz) # Provides functions for microbiome data visualization and analysis.
+library(vegan) # Community ecology package; it can be use for diverse multivariate analysis.
+library(dplyr) # A tool for working/manipulating dataframes
